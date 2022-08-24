dfac882910e3a375251bf33119c9e40f2d897570
CrashMaster8497 <crashmaster8497@gmail.com>
2022-08-24 18:01:51 +0800

    feat: add note

---
+++ src/CustomerLibrary/BusinessEntities/Note.cs
@@ -0,0 +1,8 @@
+﻿namespace CustomerLibrary.BusinessEntities
+{
+    public class Note
+    {
+        public int NoteId { get; set; } = 0;
+        public string? Text { get; set; } = null;
+    }
+}
---
+++ tests/CustomerLibrary.Tests/BusinessEntities/NoteTest.cs
@@ -0,0 +1,17 @@
+﻿using CustomerLibrary.BusinessEntities;
+
+namespace CustomerLibrary.Tests.BusinessEntities
+{
+    public class NoteTest
+    {
+        [Fact]
+        public void ShouldBeAbleToCreateNote()
+        {
+            var note = new Note();
+
+            Assert.NotNull(note);
+            Assert.Equal(0, note.NoteId);
+            Assert.Null(note.Text);
+        }
+    }
+}
